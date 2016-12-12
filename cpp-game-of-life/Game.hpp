8046fb4012efb746f2fa39afaeaceddef4496ab0
bobdesaunois <bobdesaunois@gmail.com>
2016-12-12 11:15:23 +0100

    Some changes to constants

--- cpp-game-of-life/Game.hpp
+++ cpp-game-of-life/Game.hpp
@@ -11,9 +11,9 @@ class Game
 
 public:
 
-    static const int GAME_SPEED_MILLISECONDS    = 500;      // Time the game waits before continuing to the next generation.
+    static const int GAME_SPEED_MILLISECONDS    = 250;      // Time the game waits before continuing to the next generation.
     static const int MAP_SIZE                   = 800;      // Make sure it's a number dividable by two.
-    static const int MAP_CELL_DENSITY           = 40;       // Increase this value to decrease density.
+    static const int MAP_CELL_DENSITY           = 75;       // Increase this value to decrease density.
     static const int CELLS_PER_ROW              = 40;       // The width of the map.
     
 private:
